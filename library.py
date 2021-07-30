163c91e83ca241af71a08089a85d12bb46599301
inkvizitor1991 <85788805+inkvizitor1991@users.noreply.github.com>
2021-07-30 14:06:44 +0300

    parse books genres

--- library.py
+++ library.py
@@ -17,16 +17,20 @@ def generate_books(books):
         title_text = title_tag.text
         name, *autor = title_text.split("::")
         filename = f'{book}. {name}'.rstrip()
-        print(name)
         try:
             check_for_redirect(response)
-            for teg in soup.find_all('div', class_="texts"):
-                text_comments = teg.find(class_="black").get_text()
-                print(text_comments)
+            #for teg in soup.find_all('div', class_="texts"):
+                #text_comments = teg.find(class_="black").get_text()
+                #print(text_comments)
+
+            genre= soup.find('span', class_='d_book').find_all('a')
+            text_genre=[teg.text for teg in genre]
+            print('Заголовок:', name)
+            print(text_genre)
         except:
             pass
-        download_txt(url_book, filename)
-        download_image(basic_url, soup)
+        #download_txt(url_book, filename)
+        #download_image(basic_url, soup)
 
 
 def check_for_redirect(response):
@@ -41,8 +45,8 @@ def download_txt(url_book, filename, folder='books/'):
     combined_filepath = os.path.join(folder, f'{filename}.txt')
     try:
         check_for_redirect(response)
-        with open(f'{combined_filepath}', 'wb') as file:
-            file.write(response.content)
+        #with open(f'{combined_filepath}', 'wb') as file:
+            #file.write(response.content)
     except:
         pass
 
@@ -59,8 +63,8 @@ def download_image(basic_url, soup, folder='image/'):
         parse_image_url = urlparse(relative_image_url)
         filename = parse_image_url.path.split('/')[-1]
         combined_filepath = os.path.join(folder, f'{filename}')
-        with open(f'{combined_filepath}', 'wb') as file:
-            file.write(response.content)
+        #with open(f'{combined_filepath}', 'wb') as file:
+            #file.write(response.content)
     except:
         pass
 
